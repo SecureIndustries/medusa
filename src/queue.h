b820da9dfd7b99e9733672d007d8d8efcd64f6f6
Alper Akcan <alper@secure.team>
2018-07-21 01:14:36 +0300

    add missing functions

--- src/queue.h
+++ src/queue.h
@@ -2,6 +2,81 @@
 #include <sys/queue.h>
 
 /*
+ * Singly-linked List missing functions.
+ */
+#if !defined(SLIST_EMPTY)
+#define SLIST_EMPTY(head)       ((head)->slh_first == NULL)
+#endif
+
+#if !defined(SLIST_FIRST)
+#define SLIST_FIRST(head)       ((head)->slh_first)
+#endif
+
+#if !defined(SLIST_FOREACH)
+#define SLIST_FOREACH(var, head, field)                                         \
+        for ((var) = SLIST_FIRST((head));                                       \
+            (var);                                                              \
+            (var) = SLIST_NEXT((var), field))
+#endif
+
+#if !defined(SLIST_FOREACH_SAFE)
+#define SLIST_FOREACH_SAFE(var, head, field, tvar)                              \
+        for ((var) = SLIST_FIRST((head));                                       \
+            (var) && ((tvar) = SLIST_NEXT((var), field), 1);                    \
+            (var) = (tvar))
+#endif
+
+#if !defined(SLIST_FOREACH_PREVPTR)
+#define SLIST_FOREACH_PREVPTR(var, varp, head, field)                           \
+        for ((varp) = &SLIST_FIRST((head));                                     \
+            ((var) = *(varp)) != NULL;                                          \
+            (varp) = &SLIST_NEXT((var), field))
+#endif
+
+#if !defined(SLIST_INIT)
+#define SLIST_INIT(head)
+        do {                                                                    \
+                SLIST_FIRST((head)) = NULL;                                     \
+        } while (0)
+#endif
+
+#if !defined(SLIST_INSERT_AFTER)
+#define SLIST_INSERT_AFTER(slistelm, elm, field)
+        do {                                                                    \
+                SLIST_NEXT((elm), field) = SLIST_NEXT((slistelm), field);       \
+                SLIST_NEXT((slistelm), field) = (elm);                          \
+        } while (0)
+#endif
+
+#if !defined(SLIST_INSERT_HEAD)
+#define SLIST_INSERT_HEAD(head, elm, field)
+        do {                        \
+                SLIST_NEXT((elm), field) = SLIST_FIRST((head));                 \
+                SLIST_FIRST((head)) = (elm);                                    \
+        } while (0)
+#endif
+
+#if !defined(SLIST_NEXT)
+        #define SLIST_NEXT(elm, field)  ((elm)->field.sle_next)
+#endif
+
+#if !defined(SLIST_REMOVE)
+#define SLIST_REMOVE(head, elm, type, field)                                    \
+        do {                                                                    \
+                if (SLIST_FIRST((head)) == (elm)) {                             \
+                        SLIST_REMOVE_HEAD((head), field);                       \
+                }                                                               \
+                else {                                                          \
+                        struct type *curelm = SLIST_FIRST((head));              \
+                        while (SLIST_NEXT(curelm, field) != (elm))              \
+                                curelm = SLIST_NEXT(curelm, field);             \
+                        SLIST_REMOVE_AFTER(curelm, field);                      \
+                }                                                               \
+                TRASHIT((elm)->field.sle_next);                                 \
+        } while (0)
+#endif
+
+/*
  * Tail queue missing functions.
  */
 
