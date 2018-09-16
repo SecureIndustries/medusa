1fdf8ebccb5ad82d806fbcff807e6dca415c1dba
Alper Akcan <alper@secure.team>
2018-09-16 20:34:29 +0300

    sigaction

--- src/signal-sigaction.c
+++ src/signal-sigaction.c
@@ -7,8 +7,6 @@
 #include <signal.h>
 #include <errno.h>
 
-#include <sys/signalfd.h>
-
 #include "error.h"
 #include "clock.h"
 #include "queue.h"
@@ -18,20 +16,29 @@
 #include "signal-struct.h"
 #include "signal-private.h"
 
-#include "signal-signalfd.h"
+#include "signal-sigaction.h"
 
 TAILQ_HEAD(entries, entry);
 struct entry {
         TAILQ_ENTRY(entry) list;
         struct medusa_signal *signal;
+        struct sigaction sa;
 };
 
 struct internal {
         struct medusa_signal_backend backend;
-        int sfd;
-        sigset_t sigset;
+        int sfd[2];
         struct entries entries;
 };
+
+static int g_signal_handler_wakeup_write;
+static int g_signal_handler_wakeup_fired;
+static pthread_mutex_t g_signal_handler_wakeup_mutex;
+
+static void internal_signal_handler (int number)
+{
+        (void) number;
+}
 
 static int internal_fd (struct medusa_signal_backend *backend)
 {
@@ -39,13 +46,14 @@ static int internal_fd (struct medusa_signal_backend *backend)
         if (MEDUSA_IS_ERR_OR_NULL(internal)) {
                 return -EINVAL;
         }
-        return internal->sfd;
+        return internal->sfd[0];
 }
 
 static int internal_add (struct medusa_signal_backend *backend, struct medusa_signal *signal)
 {
         int rc;
         struct entry *entry;
+        struct sigaction sa;
         struct internal *internal = (struct internal *) backend;
         if (MEDUSA_IS_ERR_OR_NULL(internal)) {
                 return -EINVAL;
@@ -56,29 +64,23 @@ static int internal_add (struct medusa_signal_backend *backend, struct medusa_si
         if (signal->number <= 0) {
                 return -EINVAL;
         }
-        rc = sigismember(&internal->sigset, signal->number);
-        if (rc < 0) {
-                return -errno;
-        } else if (rc != 0) {
-                return -EEXIST;
-        }
-        rc = sigaddset(&internal->sigset, signal->number);
-        if (rc < 0) {
-                return -errno;
-        }
-        rc = sigprocmask(SIG_BLOCK, &internal->sigset, NULL);
-        if (rc < 0) {
-                return -errno;
-        }
-        rc = signalfd(internal->sfd, &internal->sigset, SFD_NONBLOCK);
-        if (rc < 0) {
-                return -errno;
+        TAILQ_FOREACH(entry, &internal->entries, list) {
+                if (entry->signal->number == signal->number) {
+                        return -EEXIST;
+                }
         }
         entry = malloc(sizeof(struct entry));
         if (entry == NULL) {
                 return -ENOMEM;
         }
         entry->signal = signal;
+        memset(&sa, 0, sizeof(struct sigaction));
+        sa.sa_handler = internal_signal_handler;
+        rc = sigaction(signal->number, &sa, &entry->sa);
+        if (rc < 0) {
+                free(entry);
+                return -errno;
+        }
         TAILQ_INSERT_TAIL(&internal->entries, entry, list);
         return 0;
 }
@@ -86,9 +88,7 @@ static int internal_add (struct medusa_signal_backend *backend, struct medusa_si
 static int internal_del (struct medusa_signal_backend *backend, struct medusa_signal *signal)
 {
         int rc;
-        sigset_t sigset;
         struct entry *entry;
-        struct entry *nentry;
         struct internal *internal = (struct internal *) backend;
         if (MEDUSA_IS_ERR_OR_NULL(internal)) {
                 return -EINVAL;
@@ -99,35 +99,20 @@ static int internal_del (struct medusa_signal_backend *backend, struct medusa_si
         if (signal->number <= 0) {
                 return -EINVAL;
         }
-        rc = sigismember(&internal->sigset, signal->number);
-        if (rc < 0) {
-                return -errno;
-        } else if (rc == 0) {
-                return -ENOENT;
-        }
-        rc = signalfd(internal->sfd, &internal->sigset, 0);
-        if (rc < 0) {
-                return -errno;
-        }
-        sigemptyset(&sigset);
-        rc = sigaddset(&sigset, signal->number);
-        if (rc < 0) {
-                return -errno;
-        }
-        rc = sigprocmask(SIG_UNBLOCK, &sigset, NULL);
-        if (rc < 0) {
-                return -errno;
-        }
-        rc = sigdelset(&internal->sigset, signal->number);
-        if (rc < 0) {
-                return -errno;
-        }
-        TAILQ_FOREACH_SAFE(entry, &internal->entries, list, nentry) {
+        TAILQ_FOREACH(entry, &internal->entries, list) {
                 if (entry->signal->number == signal->number) {
-                        TAILQ_REMOVE(&internal->entries, entry, list);
-                        free(entry);
+                        break;
                 }
         }
+        if (entry == NULL) {
+                return -ENOENT;
+        }
+        rc = sigaction(signal->number, &entry->sa, NULL);
+        if (rc < 0) {
+                return -errno;
+        }
+        TAILQ_REMOVE(&internal->entries, entry, list);
+        free(entry);
         return 0;
 }
 
@@ -136,7 +121,7 @@ static int internal_run (struct medusa_signal_backend *backend)
         int rc;
         struct entry *entry;
         struct entry *nentry;
-        struct signalfd_siginfo signalfd_siginfo;
+        struct sigaction_siginfo sigaction_siginfo;
         struct internal *internal = (struct internal *) backend;
         if (MEDUSA_IS_ERR_OR_NULL(internal)) {
                 return -EINVAL;
@@ -144,7 +129,7 @@ static int internal_run (struct medusa_signal_backend *backend)
         if (MEDUSA_IS_ERR_OR_NULL(signal)) {
                 return -EINVAL;
         }
-        rc = read(internal->sfd, &signalfd_siginfo, sizeof(struct signalfd_siginfo));
+        rc = read(internal->sfd[0], &sigaction_siginfo, sizeof(struct sigaction_siginfo));
         if (rc < 0) {
                 if (errno == EINTR) {
                         return 0;
@@ -152,11 +137,11 @@ static int internal_run (struct medusa_signal_backend *backend)
                         return -errno;
                 }
         }
-        if (rc != sizeof(struct signalfd_siginfo)) {
+        if (rc != sizeof(struct sigaction_siginfo)) {
                 return -EIO;
         }
         TAILQ_FOREACH_SAFE(entry, &internal->entries, list, nentry) {
-                if (entry->signal->number == (int) signalfd_siginfo.ssi_signo) {
+                if (entry->signal->number == (int) sigaction_siginfo.ssi_signo) {
                         rc = medusa_signal_onevent(entry->signal, MEDUSA_SIGNAL_EVENT_FIRED);
                         if (rc < 0) {
                                 return rc;
@@ -174,8 +159,11 @@ static void internal_destroy (struct medusa_signal_backend *backend)
         if (internal == NULL) {
                 return;
         }
-        if (internal->sfd >= 0) {
-                close(internal->sfd);
+        if (internal->sfd[0] >= 0) {
+                close(internal->sfd[0]);
+        }
+        if (internal->sfd[1] >= 0) {
+                close(internal->sfd[1]);
         }
         TAILQ_FOREACH_SAFE(entry, &internal->entries, list, nentry) {
                 TAILQ_REMOVE(&internal->entries, entry, list);
@@ -184,35 +172,27 @@ static void internal_destroy (struct medusa_signal_backend *backend)
         free(internal);
 }
 
-struct medusa_signal_backend * medusa_signal_signalfd_create (const struct medusa_signal_signalfd_init_options *options)
+struct medusa_signal_backend * medusa_signal_sigaction_create (const struct medusa_signal_sigaction_init_options *options)
 {
         int rc;
         struct internal *internal;
         (void) options;
         internal = (struct internal *) malloc(sizeof(struct internal));
         if (internal == NULL) {
-                goto bail;
+                return MEDUSA_ERR_PTR(-ENOMEM);
         }
         memset(internal, 0, sizeof(struct internal));
         TAILQ_INIT(&internal->entries);
-        sigemptyset(&internal->sigset);
-        rc = sigprocmask(SIG_BLOCK, &internal->sigset, NULL);
+        rc = pipe2(internal->sfd, O_NONBLOCK);
         if (rc < 0) {
+                internal_destroy(&internal->backend);
                 return MEDUSA_ERR_PTR(-errno);
         }
-        internal->sfd = signalfd(-1, &internal->sigset, SFD_NONBLOCK);
-        if (internal->sfd < 0) {
-                goto bail;
-        }
-        internal->backend.name    = "signalfd";
+        internal->backend.name    = "sigaction";
         internal->backend.fd      = internal_fd;
         internal->backend.add     = internal_add;
         internal->backend.del     = internal_del;
         internal->backend.run     = internal_run;
         internal->backend.destroy = internal_destroy;
         return &internal->backend;
-bail:   if (internal != NULL) {
-                internal_destroy(&internal->backend);
-        }
-        return NULL;
 }
