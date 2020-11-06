328856d604b3e59b7006e888221808c1b24865ff
Murat Demirten <mdemirten@yh.com.tr>
2020-11-06 15:34:59 +0300

    version 1.0.6

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 
 #define MEDUSA_VERSION_MAJOR                            1
 #define MEDUSA_VERSION_MINOR                            0
-#define MEDUSA_VERSION_PATCH                            5
-#define MEDUSA_VERSION_STRING                           "1.0.5"
+#define MEDUSA_VERSION_PATCH                            6
+#define MEDUSA_VERSION_STRING                           "1.0.6"
 #define MEDUSA_VERSION                                  MEDUSA_VERSION_CHECK(MEDUSA_VERSION_MAJOR, MEDUSA_VERSION_MINOR, MEDUSA_VERSION_PATCH)
 #define MEDUSA_VERSION_CHECK(major, minor, patch)       ((major << 16) | (minor << 8) | (patch))
