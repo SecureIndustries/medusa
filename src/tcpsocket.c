0698ad53ef740bcbaf251509a61e483113fa76e5
Alper Akcan <alper.akcan@gmail.com>
2021-09-15 15:48:21 +0300

    tcpsocket: set tcp nodelay whet connected

--- src/tcpsocket.c
+++ src/tcpsocket.c
@@ -149,6 +149,10 @@ static inline int tcpsocket_set_state (struct medusa_tcpsocket *tcpsocket, unsig
                         }
                 }
         } else if (state == MEDUSA_TCPSOCKET_STATE_CONNECTED) {
+                rc = medusa_tcpsocket_set_nodelay_unlocked(tcpsocket, tcpsocket_has_flag(tcpsocket, MEDUSA_TCPSOCKET_FLAG_NODELAY));
+                if (rc < 0) {
+                        return rc;
+                }
                 if (!MEDUSA_IS_ERR_OR_NULL(tcpsocket->ctimer)) {
                         rc = medusa_timer_set_enabled_unlocked(tcpsocket->ctimer, 0);
                         if (rc < 0) {
@@ -2411,18 +2415,13 @@ __attribute__ ((visibility ("default"))) int medusa_tcpsocket_set_nodelay_unlock
         } else {
                 tcpsocket_del_flag(tcpsocket, MEDUSA_TCPSOCKET_FLAG_NODELAY);
         }
-        if (!MEDUSA_IS_ERR_OR_NULL(tcpsocket->io)) {
+        if (!MEDUSA_IS_ERR_OR_NULL(tcpsocket->io) &&
+            tcpsocket->state == MEDUSA_TCPSOCKET_STATE_CONNECTED) {
                 int rc;
                 int on;
                 on = !!tcpsocket_has_flag(tcpsocket, MEDUSA_TCPSOCKET_FLAG_NODELAY);
                 rc = setsockopt(medusa_io_get_fd_unlocked(tcpsocket->io), IPPROTO_TCP, TCP_NODELAY, (void *) &on, sizeof(on));
                 if (rc != 0) {
-#if defined(_WIN32)
-                        if (rc == SOCKET_ERROR) {
-                                fprintf(stderr, "tcp setsockopt tcp_nodelay error: %d\n", WSAGetLastError());
-                        }
-                        return 0;
-#endif
                         return -errno;
                 }
         }
